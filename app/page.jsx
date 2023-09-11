5b1a481beda1040f2656c4c93fe5887b4617caeb
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-09-11 20:03:18 +0300

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="capitalize flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <Link
+          href="/quiz"
+          className="group rounded-lg items-center border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            quiz{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </Link>
+        <Link
+          href="https://docs.google.com/presentation/d/15UCbPsOmazsDAL-nf4jXLJKfT699EFiFR-DAC-sLKjo/edit?usp=sharing"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            slides{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </Link>
+      </div>
+    </main>
+  );
+}
---
+++ app/quiz/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Quiz() {
+  return (
+    <iframe
+      src="https://wordwall.net/embed/a3ccfa88ffcd479a93800f1a1cf9b2f9?themeId=49&templateId=5&fontStackId=0"
+      width="100%"
+      height="700px"
+      allowfullscreen
+    ></iframe>
+  );
+}
